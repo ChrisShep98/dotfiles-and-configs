39b4061be76cc55732834c01d21f17932c80d467
ChrisShep98 <shepardchristopher1@gmail.com>
2026-06-29 20:58:58 -0400

    no need to add those mappings cuz they are not custom

--- nvim/lua/plugins/neoscroll.lua
+++ nvim/lua/plugins/neoscroll.lua
@@ -4,12 +4,6 @@ return {
     opts = {},
     config = function()
       require("neoscroll").setup({
-        mappings = { -- Keys to be mapped to their corresponding default scrolling animation
-          '<C-u>', '<C-d>',
-          '<C-b>', '<C-f>',
-          '<C-y>', '<C-e>',
-          'zt', 'zz', 'zb',
-        },
         hide_cursor = false,
       })
     end
